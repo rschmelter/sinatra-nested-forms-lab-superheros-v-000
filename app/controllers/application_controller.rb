897845e81eae1968f06fee9328ab801315c4f414
Ryan Schmelter <rschmelter33@gmail.com>
2018-03-02 20:15:20 +0000

    automatically backed up by learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,7 +5,7 @@ class App < Sinatra::Base
     set :views, Proc.new { File.join(root, "../views/") }
 
   get '/' do
-    erb :'app/views/super_hero.erb'
+    erb :'app/super_hero.erb'
   end
-    
+
 end
